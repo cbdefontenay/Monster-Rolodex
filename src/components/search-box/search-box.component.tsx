9e8ac478b7057c480fa0eef64851eb8886323534
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-02-16 22:52:02 +0100

    adding some typescript

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,21 @@
+import './search-box.styles.css';
+
+interface ISearchBoxProps {
+  className: string;
+  placeholder?: string;
+}
+
+interface ISearchBoxProps {
+  onChangeHandler: (a: string) => void
+}
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
