7fc2c657352803463f46fed15765d56945f8e59c
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-03-03 20:14:57 +0100

    ...

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,17 +1,20 @@
-import { ChangeEvent } from 'react';
-
-import './search-box.styles.css';
+import { ChangeEvent } from "react";
+import "./search-box.styles.css";
 
 type SearchBoxProps = {
   className: string;
   placeholder?: string;
-  onChangeHandler: ( event: ChangeEvent<HTMLInputElement>) => void;
-}
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: SearchBoxProps) => (
   <input
     className={`search-box ${className}`}
-    type='search'
+    type="search"
     placeholder={placeholder}
     onChange={onChangeHandler}
   />
